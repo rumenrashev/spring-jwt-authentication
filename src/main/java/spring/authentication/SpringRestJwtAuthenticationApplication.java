34cd573eb02dd4170f7fdc8583b4c67c73ba54b5
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2021-06-29 10:17:59 +0300

    create project, add dependencies

---
+++ src/main/java/spring/authentication/SpringRestJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package spring.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestJwtAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestJwtAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/authentication/SpringRestJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestJwtAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
